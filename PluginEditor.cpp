e5ce8a599759260a63c800227b771c3540f7e198
Brady Boettcher <bboettcher2@wisc.edu>
2017-07-16 22:39:53 -0500

    polyphony almost done
    ~NOT YET FUNCTIONAL~

--- PluginEditor.cpp
+++ PluginEditor.cpp
@@ -20,70 +20,93 @@ FirstPluginAudioProcessorEditor::FirstPluginAudioProcessorEditor(FirstPluginAudi
     // editor's size to whatever you need it to be.
     setSize (800, 500);
 
-	//set osc1 amplitude parameters
-	osc1Amp.setSliderStyle(Slider::RotaryHorizontalDrag);
-	osc1Amp.setRange(0, 127, 1);
-	osc1Amp.setTextBoxStyle(Slider::NoTextBox, false, 90, 0);
-	osc1Amp.setPopupDisplayEnabled(false, this);
-	osc1Amp.setValue(1);
-	addAndMakeVisible(&osc1Amp);
-	osc1Amp.setLookAndFeel(&customLookAndFeel);
-	//set osc1 octave parameters
-	osc1Octave.setSliderStyle(Slider::RotaryHorizontalDrag);
-	osc1Octave.setRange(-3, 3, 1);
-	osc1Octave.setTextBoxStyle(Slider::NoTextBox, false, 90, 0);
-	osc1Octave.setPopupDisplayEnabled(true, this);
-	osc1Octave.setValue(0);
-	addAndMakeVisible(&osc1Octave);
-	osc1Octave.setLookAndFeel(&customLookAndFeel);
-	//set osc1 wave parameters
-	osc1Wave.setSliderStyle(Slider::RotaryHorizontalDrag);
-	osc1Wave.setRange(0, 3, 1);
-	osc1Wave.setTextBoxStyle(Slider::NoTextBox, false, 90, 0);
-	osc1Wave.setPopupDisplayEnabled(false, this);
-	osc1Wave.setValue(0);
-	addAndMakeVisible(&osc1Wave);
-	osc1Wave.setLookAndFeel(&customLookAndFeel);
-	//set osc1 semitone parameters
-	osc1Semi.setSliderStyle(Slider::RotaryHorizontalDrag);
-	osc1Semi.setRange(-11, 11, 1);
-	osc1Semi.setTextBoxStyle(Slider::NoTextBox, false, 90, 0);
-	osc1Semi.setPopupDisplayEnabled(true, this);
-	osc1Semi.setValue(0);
-	addAndMakeVisible(&osc1Semi);
-	osc1Semi.setLookAndFeel(&customLookAndFeel);
-	//set osc1 fine parameters
-	osc1Fine.setSliderStyle(Slider::RotaryHorizontalDrag);
-	osc1Fine.setRange(-99, 99, 1);
-	osc1Fine.setTextBoxStyle(Slider::NoTextBox, false, 90, 0);
-	osc1Fine.setPopupDisplayEnabled(true, this);
-	osc1Fine.setValue(0);
-	addAndMakeVisible(&osc1Fine);
-	osc1Fine.setLookAndFeel(&customLookAndFeel);
-
-	addAndMakeVisible(debug);
-	debug.setText("TESTINGGGG", dontSendNotification);
-	debug.setColour(Label::textColourId, Colours::black);
-	osc1Amp.addListener(this);
-	
+	initOscillators();
+	initEnvelopes();
 
 	//set matrix logo parameters
 	File logoPath = "C:/Users/Asus1/Documents/JUCE/projects/FirstPlugin/Resources/logoCrop.png";
 	logo = ImageFileFormat::loadFrom(logoPath);
 
-	//add listener to slider
-	osc1Amp.addListener(this);
-
 	//timer for updating GUI
 	startTimerHz(30);
+
 }
 
 FirstPluginAudioProcessorEditor::~FirstPluginAudioProcessorEditor()
 {
 }
 
+void FirstPluginAudioProcessorEditor::comboBoxChanged(ComboBox* comboBox) {
+	for (int i = 0; i < processor.numVoices; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (comboBox == &oscillatorSliders[i].wave) {
+				switch (oscillatorSliders[i].wave.getSelectedId()) {
+				case 1: 
+					processor.voices[i].osc[j].wave = processor.SINE;
+					break;
+				case 2: 
+					processor.voices[i].osc[j].wave = processor.SAW;
+					break;
+				case 3: 
+					processor.voices[i].osc[j].wave = processor.SQUARE;
+					break;
+				case 4: 
+					processor.voices[i].osc[j].wave = processor.NOISE;
+					break;
+				case 5:
+					processor.voices[i].osc[j].wave = processor.TRIANGLE;
+					break;
+				}
+			}
+			processor.voices[i].osc[j].osc.phaseReset(0);
+		}
+		
+		
+	}
+	
+}
+
 void FirstPluginAudioProcessorEditor::sliderValueChanged(Slider* slider) {
-	//debug.setText((String)processor.osc1Env.trigger, dontSendNotification);
+
+		for (int i = 0; i < 3; i++) {
+			if (slider == &oscillatorSliders[i].amplitude) {
+				processor.masterOsc[i].amplitude = oscillatorSliders[i].amplitude.getValue();
+			}
+			else if (slider == &oscillatorSliders[i].octave) {
+				processor.masterOsc[i].octave = oscillatorSliders[i].octave.getValue();
+			}
+			else if (slider == &oscillatorSliders[i].semi) {
+				processor.masterOsc[i].semi = oscillatorSliders[i].semi.getValue();
+			}
+			else if (slider == &oscillatorSliders[i].fine) {
+				processor.masterOsc[i].fine = oscillatorSliders[i].fine.getValue();
+			}
+			else if (slider == &envelopeSliders[i].attack) {
+				processor.masterEnv.env.setAttack(envelopeSliders[i].attack.getValue());
+			}
+			else if (slider == &envelopeSliders[i].decay) {
+				processor.masterEnv.env.setDecay(envelopeSliders[i].decay.getValue());
+			}
+			else if (slider == &envelopeSliders[i].sustain) {
+				processor.masterEnv.env.setSustain(envelopeSliders[i].sustain.getValue());
+			}
+			else if (slider == &envelopeSliders[i].release) {
+				processor.masterEnv.env.setRelease(envelopeSliders[i].release.getValue());
+			}
+		}
+
+	
+
+}
+
+void FirstPluginAudioProcessorEditor::buttonClicked(Button *button) {
+	for (int i = 0; i < 3; i++) {
+		if (button == &envelopeSliders[i].enabled) {
+			processor.masterEnv.enabled = envelopeSliders[i].enabled.getState();
+			break;
+		}
+		
+	}
 }
 
 //==============================================================================
@@ -97,14 +120,41 @@ void FirstPluginAudioProcessorEditor::paint (Graphics& g)
     g.setColour (Colours::midnightblue);
 	//g.setOpacity(.7f);
 
+	//oscillator text
 	g.setFont(15.0f);
-	g.drawFittedText("Amp", 135, 75, 100, 30, Justification::centred, 1);
-	g.drawFittedText("Wave", -25, 75, 100, 30, Justification::centred, 1);
-	g.drawFittedText("Octave", 20, 75, 100, 30, Justification::centred, 1);
-	g.drawFittedText("Semi", 60, 75, 100, 30, Justification::centred, 1);
-	g.drawFittedText("Fine", 98, 75, 100, 30, Justification::centred, 1);
+	g.drawFittedText("Octave", 85, 100, 100, 30, Justification::centred, 1);
+	g.drawFittedText("Semi", 135, 100, 100, 30, Justification::centred, 1);
+	g.drawFittedText("Fine", 185, 100, 100, 30, Justification::centred, 1);
+	g.drawFittedText("Amp", 235, 100, 100, 30, Justification::centred, 1);
+	g.drawFittedText("Octave", 85, 200, 100, 30, Justification::centred, 1);
+	g.drawFittedText("Semi", 135, 200, 100, 30, Justification::centred, 1);
+	g.drawFittedText("Fine", 185, 200, 100, 30, Justification::centred, 1);
+	g.drawFittedText("Amp", 235, 200, 100, 30, Justification::centred, 1);
+	g.drawFittedText("Octave", 85, 300, 100, 30, Justification::centred, 1);
+	g.drawFittedText("Semi", 135, 300, 100, 30, Justification::centred, 1);
+	g.drawFittedText("Fine", 185, 300, 100, 30, Justification::centred, 1);
+	g.drawFittedText("Amp", 235, 300, 100, 30, Justification::centred, 1);
 
-	//g.drawFittedText(osc1EnvVal.toString(), 98, 75, 100, 30, Justification::centred, 1);
+	//envelope text
+	g.drawFittedText("A", 350, 295, 100, 30, Justification::centred, 1);
+	g.drawFittedText("D", 400, 295, 100, 30, Justification::centred, 1);
+	g.drawFittedText("S", 450, 295, 100, 30, Justification::centred, 1);
+	g.drawFittedText("R", 500, 295, 100, 30, Justification::centred, 1);
+	g.drawFittedText("Amplitude Envelope", 580, 265, 140, 30, Justification::centred, 1);
+
+	//envelope text
+	g.drawFittedText("A", 350, 375, 100, 30, Justification::centred, 1);
+	g.drawFittedText("D", 400, 375, 100, 30, Justification::centred, 1);
+	g.drawFittedText("S", 450, 375, 100, 30, Justification::centred, 1);
+	g.drawFittedText("R", 500, 375, 100, 30, Justification::centred, 1);
+	//g.drawFittedText("Amplitude Envelope", 580, 265, 140, 30, Justification::centred, 1);
+
+	//envelope text
+	g.drawFittedText("A", 350, 455, 100, 30, Justification::centred, 1);
+	g.drawFittedText("D", 400, 455, 100, 30, Justification::centred, 1);
+	g.drawFittedText("S", 450, 455, 100, 30, Justification::centred, 1);
+	g.drawFittedText("R", 500, 455, 100, 30, Justification::centred, 1);
+	//g.drawFittedText("Amplitude Envelope", 580, 265, 140, 30, Justification::centred, 1);
 
 	g.setFont(15.0f);
 	g.drawFittedText("File Management", 250, 10, 100, 30, Justification::centred, 1);
@@ -116,15 +166,11 @@ void FirstPluginAudioProcessorEditor::paint (Graphics& g)
 	//draw rounded rect
 	g.drawRoundedRectangle(615, 5, 180, 35, 10.0f, 2.0f);
 
-	//g.setFont (15.0f);
-	//g.drawFittedText ("Oscillators", 100, 50, 100, 30, Justification::centred, 1);
-	//draw rounded rect
-	g.drawRoundedRectangle(5, 45, 200, 300, 10.0f, 2.0f);
+	//draw rounded rect (oscillators)
+	g.drawRoundedRectangle(5, 45, 305, 300, 10.0f, 2.0f);
 
-	g.setFont(15.0f);
-	g.drawFittedText("Oscilloscope", 220, 50, 100, 30, Justification::centred, 1);
 	//draw rounded rect
-	g.drawRoundedRectangle(210, 45, 400, 200, 10.0f, 2.0f);
+	//g.drawRoundedRectangle(210, 45, 400, 200, 10.0f, 2.0f);
 
 	g.setFont(15.0f);
 	g.drawFittedText("Filters", 100, 355, 100, 30, Justification::centred, 1);
@@ -132,14 +178,14 @@ void FirstPluginAudioProcessorEditor::paint (Graphics& g)
 	g.drawRoundedRectangle(5, 350, 305, 145, 10.0f, 2.0f);
 
 	g.setFont(15.0f);
-	g.drawFittedText("Envelopes", 350, 360, 100, 30, Justification::centred, 1);
+	//g.drawFittedText("Envelopes", 350, 360, 100, 30, Justification::centred, 1);
 	//draw rounded rect
 	g.drawRoundedRectangle(315, 250, 480, 245, 10.0f, 2.0f);
 
 	g.setFont(15.0f);
-	g.drawFittedText("LFO", 200, 255, 100, 30, Justification::centred, 1);
+	g.drawFittedText("LFO", 3350, 60, 100, 30, Justification::centred, 1);
 	//draw rounded rect
-	g.drawRoundedRectangle(210, 250, 100, 95, 10.0f, 2.0f);
+	g.drawRoundedRectangle(315, 45, 295, 200, 10.0f, 2.0f);
 
 	g.setFont(15.0f);
 	g.drawFittedText("Volume Controls", 620, 50, 100, 30, Justification::centred, 1);
@@ -152,16 +198,13 @@ void FirstPluginAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
-	osc1Fine.setBounds(130, 50, 35, 35);
-	osc1Octave.setBounds(50, 50, 35, 35);
-	osc1Wave.setBounds(10, 50, 35, 35);
-	osc1Semi.setBounds(90, 50, 35, 35);
-	osc1Amp.setBounds(170, 50, 35, 35);
-	debug.setBounds(250, 200, 200, 200);
+	initBounds();
+	
+	debug.setBounds(350, 300, 200, 200);
 }
 
 void FirstPluginAudioProcessorEditor::updateGUI() {
-	debug.setText((String)(processor.osc1Val), dontSendNotification);
+	debug.setText((String)(processor.voices[0].env.amplitude), dontSendNotification);
 }
 
 //==============================================================================
--- PluginProcessor.cpp
+++ PluginProcessor.cpp
@@ -24,12 +24,8 @@ FirstPluginAudioProcessor::FirstPluginAudioProcessor()
                        )
 #endif
 {
-	osc1Env.setAttack(100);
-	osc1Env.setDecay(10);
-	osc1Env.setSustain(1);
-	osc1Env.setRelease(100);
-	osc1Env.amplitude = 0;
-	//editor = new FirstPluginAudioProcessorEditor(*this);
+	//envArray[0].env.amplitude = 0;
+	numVoices = 0;
 }
 
 FirstPluginAudioProcessor::~FirstPluginAudioProcessor()
@@ -135,31 +131,80 @@ void FirstPluginAudioProcessor::processBlock (AudioSampleBuffer& buffer, MidiBuf
 	//buffer.clear();
 	int time;
 	MidiMessage m;
-	float curSampleVal;
+	float curSampleVal = 0;
 	
-	//maxiEnv osc1Env;
+
 	int numSamples = buffer.getNumSamples();
 	int curSample = 0;
 	for (MidiBuffer::Iterator i(midiMessages); i.getNextEvent(m, time);) {
 		if (m.isNoteOn()) {
-			osc1Env.trigger = 1;
-			osc1Freq = m.getMidiNoteInHertz(m.getNoteNumber());
+			if (numVoices <= 10) {
+				//add voice, set trigger
+				voices[numVoices].env.amplitude = 0;
+				voices[numVoices].env.setAttack(masterEnv.env.attackms); //like this but in MS
+				voices[numVoices].env.setDecay(masterEnv.env.decayms); //like this but in MS
+				voices[numVoices].env.setSustain(masterEnv.env.sustain); //like this but in MS
+				voices[numVoices].env.setRelease(masterEnv.env.releasems); //like this but in MS
+				voices[numVoices].env.trigger = 1; //TODO: Set universal envelope params
+				voices[numVoices].midiNum = m.getNoteNumber();
+				//old code
+				//envArray[0].env.trigger = 1;
+				for (int i = 0; i < 3; i++) {
+					//set freq of voice
+					voices[numVoices].osc[i].freq = m.getMidiNoteInHertz(m.getNoteNumber() + masterOsc[i].semi + 12 * masterOsc[i].octave) + 20 * masterOsc[i].fine;
+					voices[numVoices].osc[i].wave = masterOsc[i].wave;
+					voices[numVoices].osc[i].amplitude = masterOsc[i].amplitude;
+					//old code
+					//oscArray[j].freq = m.getMidiNoteInHertz(m.getNoteNumber() + oscArray[j].semi + 12*oscArray[j].octave) + 20*oscArray[j].fine;
+				}
+				numVoices++;
+			}
+			
 		}
 		else if (m.isNoteOff()) {
-			osc1Env.trigger = 0;
+			//remove voice based on freq
+			for (int i = 0; i < numVoices; i++) {
+				if (m.getNoteNumber() == voices[i].midiNum) {
+					voices[i].env.trigger = 0;
+				}
+			}
+			shiftArray();
+			numVoices--;
+			
+			//debugInt = m.getNoteNumber() == voices[0].mi;
+			//envArray[0].env.trigger = 0;
 		}
 	}
 	
 	while (--numSamples >= 0)
 	{
-		osc1EnvVal = osc1Env.adsr(1, osc1Env.trigger);
-		osc1Val = osc1.sinewave(osc1Freq);
-		curSampleVal = osc1Val * osc1EnvVal;
+		curSampleVal = 0;
+		//make for loop go through all voices outside of this one
+		for (int i = 0; i < numVoices; i++) {
+			for (int j = 0; j < 3; j++) { 
+				switch (voices[i].osc[j].wave) { 
+				case SINE: curSampleVal += voices[i].osc[j].osc.sinewave(voices[i].osc[j].freq) * voices[i].osc[j].amplitude;
+					break;
+				case SAW: curSampleVal += voices[i].osc[j].osc.saw(voices[i].osc[j].freq) * voices[i].osc[j].amplitude;
+					break;
+				case NOISE: curSampleVal += voices[i].osc[j].osc.noise() * voices[i].osc[j].amplitude;
+					break;
+				case SQUARE: curSampleVal += voices[i].osc[j].osc.square(voices[i].osc[j].freq) * voices[i].osc[j].amplitude;
+					break;
+				case TRIANGLE: curSampleVal += voices[i].osc[j].osc.triangle(voices[i].osc[j].freq) * voices[i].osc[j].amplitude;
+					break;
+				}
+			}
+			curSampleVal *= voices[i].env.adsr(1, voices[i].env.trigger);
+		}
+		
+		//outside of osc loop but inside voice loop, mult env by curSample
+		//masterEnvVal = envArray[0].env.adsr(1, envArray[0].env.trigger);
+		//curSampleVal *= masterEnvVal;
 		for (int i = buffer.getNumChannels(); --i >= 0;) {
 			buffer.addSample(i, curSample, curSampleVal);
 		}
 		curSample++;
-		
 	}
 	
 }
@@ -195,4 +240,12 @@ void FirstPluginAudioProcessor::setStateInformation (const void* data, int sizeI
 AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 {
     return new FirstPluginAudioProcessor();
+}
+
+void FirstPluginAudioProcessor::shiftArray() 
+{
+	mtxVoice tempVoice;
+	for (int i = 0; i < numVoices; i++) {
+		voices[i] = voices[i + 1];
+	}
 }
